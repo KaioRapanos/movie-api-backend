7a64cb91eb3b12943c7c0b8d81e4d47743e76137
KaioRapanos <kaiorapanos@gmail.com>
2023-08-06 08:50:51 -0300

    Construction get and post method

---
+++ src/test/java/dev/kaio/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kaio.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
